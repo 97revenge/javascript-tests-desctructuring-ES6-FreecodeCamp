e1ffda357979e8f02f7e2c06b41723e36cf7d44c
97revenge <empregos97@gmail.com>
2022-07-13 12:01:25 -0300

    1 file modified - TEST  #1

---
+++ index.js
@@ -0,0 +1,22 @@
+let a = 8,
+  b = 6;
+
+[a, b] = [8, 6];
+
+console.log(a, b);
+
+// Altere apenas o código abaixo desta linha
+
+// BASE DE TESTES ANTIGOS :
+
+// const [b  = 8, a = 6 ] = [a ,b];
+// console.log(a);
+
+// BLOCO DE FUNÇÃO DE TESTE -
+// function swipeLetters(a, b) {
+//   const result = a == b && b == a;
+
+//   return result;
+// }
+
+// swipeLetters(2, 5);
